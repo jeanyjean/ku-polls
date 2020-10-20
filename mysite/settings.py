d3d9cdef392906a1db93806b905e64d0e8631432
jeanyjean <purich.double@gmail.com>
2020-10-20 21:59:39 +0700

    Add login page and link logout

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,3 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTHENTICATION_BACKENDS = (
+    # username/password authentication
+    'django.contrib.auth.backends.ModelBackend',  
+)
+
+LOGIN_REDIRECT_URL = '/polls/'
+
+LOGOUT_REDIRECT_URL = '/polls/'
